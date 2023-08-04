6ffa21c08d7028429c2374a74616ae371aeecf8a
Andrei Ballico <andrei.ballico@outlook.com>
2023-08-03 22:09:42 -0300

    data validation

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -3,10 +3,31 @@ import mongoose from "mongoose";
 const livroSchema = new mongoose.Schema(
   {
     id: {type: String},
-    titulo: {type: String, required: [true, "O titulo do livro é obrigatorio"]},
-    autor: {type: mongoose.Schema.Types.ObjectId, ref: "autores", required: [true, "O autor [e obrigatorio"]},
-    editora: {type: String, required: [true, "A editora é obrigatorio"]},
-    numeroPaginas: {type: Number}
+    
+    titulo: {type: String, 
+      required: [true, "O titulo do livro é obrigatorio"]},
+    
+    autor: {type: mongoose.Schema.Types.ObjectId, 
+      ref: "autores", 
+      required: [true, "O autor [e obrigatorio"]},
+      
+    editora: {type: String, 
+      required: [true, "A editora é obrigatorio"],
+      enum: {
+        values: ["Casa do código", "Alura"],
+        message: "A editora {VALUE} não é um valor permitido"
+      }
+    },
+    
+    numeroPaginas: {
+      type: Number,
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message: "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"
+      }
+    }
   }
 );
 
